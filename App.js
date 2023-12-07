502a4ce7971c014c02fcaa0236a28b29fe6a95fd
SaherRaza <saherraza7@gmail.com>
2023-12-08 00:18:46 +1100

    first commit

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import WelcomeScreen from './src/screens/OnBoardingScreens/WelcomeScreen';
+import SlidingScreens from './src/screens/OnBoardingScreens/SlidingScreens';
 
 
 export default function App() {
   return (
     <>
       <StatusBar style="auto" />
-      <WelcomeScreen />
+      <SlidingScreens />
     </>
   );
 }
--- src/screens/OnBoardingScreens/SlidingScreens.js
+++ src/screens/OnBoardingScreens/SlidingScreens.js
@@ -1,14 +1,49 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
 const SlidingScreens = () => {
   return (
-    <View>
-      <Text>SlidingScreen1</Text>
+    <View style={styles.container}>
+
+    <View style={styles.textContainer}>
+      <Text style={styles.textStyle}>Discover Something new</Text>
+      <Text style={[styles.textStyle,{fontWeight:"500", fontSize:14}]}>Special new arrivals just for you</Text>
+    </View>
+    <View style={styles.imgContainer}>
+      <Image resizeMode='contain' 
+      source={require("../../../assets/images/image1.png")}
+       />
+    </View>
+    <View style={{flex: 1, // This makes the bottom half take up the other half of the container
+    backgroundColor: '#yourColorCode', // Replace with the color code you want
+    justifyContent: 'center',
+    alignItems: 'center',
+    // Add additional styling as needed
+    }}>
+    </View>
     </View>
   )
 }
 
 export default SlidingScreens
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container:{
+    flex: 1, // This makes the top half take up half of the container
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  textContainer:{
+    marginTop:130,
+    gap:20
+  },
+  textStyle:{
+    fontSize:20,
+    textAlign:"center",
+    fontWeight:"bold"
+  },
+  imgContainer:{
+    marginTop:50
+  }
+})
