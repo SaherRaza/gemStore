35327da6a6914e1b2fbcca7bdf3199467c47907f
SaherRaza <saherraza7@gmail.com>
2024-08-15 00:36:51 +1000

    commit changes

--- src/screens/CartScreen/CartScreen.tsx
+++ src/screens/CartScreen/CartScreen.tsx
@@ -1,10 +1,12 @@
 import { StyleSheet, Text, View, TouchableOpacity, Image } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import { AntDesign } from "@expo/vector-icons";
 import CustomButton from './../../components/CustomButton';
+import Checkbox from 'expo-checkbox';
 
 const CartScreen = ({ navigation }) =>
 {
+  const [isChecked, setChecked] = useState(false);
   return (
     <View style={styles.container}>
       <View style={styles.headerStyle}>
@@ -29,14 +31,45 @@ const CartScreen = ({ navigation }) =>
           <Image style={styles.image} resizeMode="cover" source={require("../../../assets/images/blueSweater.jpg")} />
         </View>
         <View style={styles.textSection}>
-          <Text style={[styles.textStyle, { lineHeight: 40 }]}>SportWear Set</Text>
-          <Text style={[styles.textStyle, { lineHeight: 40 }]}>$ 80.00</Text>
-          <Text style={[styles.textStyle, { lineHeight: 40 }]}>Size:L | Color:Cream</Text>
+          <View style={styles.checkboxContainer}>
+            <Text style={[styles.textStyle, { lineHeight: 35, fontSize: 14 }]}>SportWear Set</Text>
+            <Checkbox
+              style={styles.checkbox}
+              value={isChecked}
+              onValueChange={setChecked}
+              color={isChecked ? '#508A7B' : "#8A8A8F"}
+            />
+          </View>
+          <Text style={[styles.textStyle, { lineHeight: 35, fontSize: 14 }]}>$ 80.00</Text>
+          <View style={styles.sizeContainer}>
+            <Text style={[styles.textStyle, { color: "#8A8A8F", fontWeight: "400", fontSize: 12, lineHeight: 35 }]}>Size:L | Color:Cream</Text>
+            <View style={styles.btn}>
+              <TouchableOpacity><Text>-</Text></TouchableOpacity>
+              <Text>0</Text>
+              <TouchableOpacity><Text>+</Text></TouchableOpacity>
+            </View>
+          </View>
         </View>
       </View>
 
-      <View style={styles.btnContainer}>
-        <CustomButton title="Proceed to Checkout" Width={"80%"} />
+      <View style={styles.bottomContainer}>
+        <View style={styles.title}>
+          <Text style={[styles.textStyle, { color: "#8A8A8F" }]}>Product Price</Text>
+          <Text style={[styles.textStyle, { fontWeight: "400" }]}>$110</Text>
+        </View>
+        <View style={styles.lineBreak} />
+        <View style={styles.title}>
+          <Text style={[styles.textStyle, { color: "#8A8A8F" }]}>Shipping</Text>
+          <Text style={[styles.textStyle, { fontWeight: "400" }]}>Freeship</Text>
+        </View>
+        <View style={styles.lineBreak} />
+        <View style={styles.title}>
+          <Text style={[styles.textStyle, { fontWeight: "500" }]}>Subtotal</Text>
+          <Text style={[styles.textStyle, { fontWeight: "500" }]}>$110</Text>
+        </View>
+        <View style={styles.btnContainer}>
+          <CustomButton title="Proceed to Checkout" Width={"80%"} />
+        </View>
       </View>
     </View>
   );
@@ -47,7 +80,7 @@ export default CartScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "white",
+    backgroundColor: "#E5E5E5",
   },
   headerStyle: {
     flexDirection: "row",
@@ -76,7 +109,7 @@ const styles = StyleSheet.create({
     width: 32, // This view will be the same size as the iconStyle view
   },
   cartListStyle: {
-    width: "85%",
+    width: "90%",
     height: "15%",
     alignSelf: "center",
     borderRadius: 20,
@@ -87,7 +120,7 @@ const styles = StyleSheet.create({
   imageSection: {
     borderTopLeftRadius: 20,
     borderBottomLeftRadius: 20,
-    width: "40%",
+    width: "35%",
     height: "100%",
     overflow: "hidden"
   },
@@ -99,11 +132,53 @@ const styles = StyleSheet.create({
   },
   textSection: {
     padding: 15,
+    flex: 1
   },
   btnContainer: {
     alignItems: "center",
     position: 'absolute',
     bottom: 20,
     width: "100%"
+  },
+  bottomContainer: {
+    backgroundColor: "#F1F1F1",
+    height: "40%",
+    position: 'absolute',
+    bottom: 0,
+    width: "100%",
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+
+  },
+  title: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    margin: 35
+  },
+  lineBreak: {
+    height: 1,
+    width: '85%',
+    backgroundColor: '#8A8A8F',
+    alignSelf: "center"
+  },
+  checkboxContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between"
+  },
+  btn: {
+    flexDirection: "row",
+    width: 90,
+    borderRadius: 40,
+    borderWidth: 1.2,
+    padding: 5,
+    justifyContent: "space-evenly",
+    borderColor: "#8A8A8F",
+    alignItems: "center"
+  },
+  sizeContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
   }
 });
