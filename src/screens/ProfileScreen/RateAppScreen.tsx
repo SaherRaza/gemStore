c26becc163363c553ec3ed0fb23b08a49941172b
SaherRaza <saherraza7@gmail.com>
2024-10-27 11:21:24 +1100

    commit changes

--- src/screens/ProfileScreen/RateAppScreen.tsx
+++ src/screens/ProfileScreen/RateAppScreen.tsx
@@ -1,5 +1,9 @@
-import { Keyboard, SafeAreaView, StyleSheet, Text, TouchableWithoutFeedback, View, TextInput } from 'react-native';
-import React from 'react';
+import
+{
+    Keyboard, SafeAreaView, StyleSheet,
+    Text, TouchableWithoutFeedback, View, TextInput, Modal, TouchableOpacity, Image
+} from 'react-native';
+import React, { useState } from 'react';
 import CustomStarRating from '../../components/CustomStarRating';
 import ScreenHeader from '../../components/ScreenHeader';
 import { NavigationProp, useNavigation } from '@react-navigation/native';
@@ -10,6 +14,20 @@ const RateAppScreen = () =>
 {
     const navigation = useNavigation<NavigationProp<ProfileParamList>>();
     const [inputValue, setInputValue] = React.useState('');
+    const [isModalVisible, setIsModalVisible] = useState(false);
+
+    const handleSendFeedback = () =>
+    {
+        // show modal when button is pressed
+        setIsModalVisible(true);
+    };
+
+    const closeModal = () =>
+    {
+        setIsModalVisible(false);
+        setInputValue('');  // Reset the input value when modal closes
+    };
+
     const maxLength = 50;  // Set your character limit here
 
     return (
@@ -38,8 +56,26 @@ const RateAppScreen = () =>
                     </Text>
                 </View>
                 <View style={{ margin: 30 }}>
-                    <CustomButton title='Send feedback' onPress={() => ""} />
+                    <CustomButton title='Send feedback' onPress={handleSendFeedback} />
                 </View>
+
+                {/* Modal */}
+                <Modal
+                    transparent={true}
+                    visible={isModalVisible}
+                    animationType="slide"
+                    onRequestClose={closeModal}
+                >
+                    <View style={styles.modalBackground}>
+                        <View style={styles.modalContainer}>
+                            <Image resizeMode='contain'
+                                source={require("../../../assets/images/Check.png")} />
+                            <Text style={styles.modalText}>Thank you for your feedback!</Text>
+                            <Text style={{ color: "#6E768A", fontSize: 15 }}>We appreciate your feedback. We'll use your feedback to improve your experience.</Text>
+                            <CustomButton Width={200} title='Done' onPress={closeModal} />
+                        </View>
+                    </View>
+                </Modal>
             </View>
         </TouchableWithoutFeedback>
     );
@@ -80,5 +116,25 @@ const styles = StyleSheet.create({
         marginTop: -35,
         fontSize: 12,
         padding: 10
+    },
+    modalBackground: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    },
+    modalContainer: {
+        width: '85%',
+        backgroundColor: 'white',
+        padding: 30,
+        borderRadius: 10,
+        alignItems: 'center',
+        height: "40%",
+        gap: 30
+    },
+    modalText: {
+        fontSize: 18,
+        fontWeight: '600',
+        marginBottom: 15,
     },
 });
